a22adadefe586d8eb72a1262306687765a02a1ec
Eudesx <eudesvicente@rocketmail.com>
2024-02-22 22:01:02 -0300

    Python

---
+++ ex003.py
@@ -0,0 +1,9 @@
+numero1 = int(input('Digite um número: '))
+numero2 = int(input('Digite outro: '))
+
+soma = numero1 + numero2
+print('A soma entre {} e {} e igual á {}.'.format(numero1, numero2, soma))
+
+
+
+
