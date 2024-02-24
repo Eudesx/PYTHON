8bd7cc2ad55890544d32b47ea7f0eb233233e25d
Eudesx <eudesvicente@rocketmail.com>
2024-02-24 20:24:37 -0300

    Python

---
+++ ex006.py
@@ -0,0 +1,10 @@
+n1 = int(input('Digite um valor: '))
+
+dobro = n1 * 2
+triplo = n1 * 3
+raiz = n1 ** 2
+
+print('O valor digitado foi {} seu dobro e {} seu triplo e {} sua raiz quadrada e {}!'.format(n1, dobro, triplo, raiz))
+
+
+
