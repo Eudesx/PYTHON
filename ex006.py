53d84809318472c15f87730a47e6079f12b7fb0d
Eudesx <eudesvicente@rocketmail.com>
2024-02-26 20:41:40 -0300

    Python

--- ex006.py
+++ ex006.py
@@ -2,9 +2,12 @@ n1 = int(input('Digite um valor: '))
 
 dobro = n1 * 2
 triplo = n1 * 3
-raiz = n1 ** 2
+raiz = n1 ** (1/2)
+print('O valor digitado foi {} \nseu dobro e {} \nseu triplo e {} \nsua raiz quadrada e {:.2f}!'.format(n1, (n1*2), (n1*3), (n1**(1/2))))
 
-print('O valor digitado foi {} seu dobro e {} seu triplo e {} sua raiz quadrada e {}!'.format(n1, dobro, triplo, raiz))
+# Pode-se fazer também com pow(n1, (1/2)
+
+
 
 
 
