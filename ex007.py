a89283979e217a8af05304b133ad44bee76c02b6
Eudesx <eudesvicente@rocketmail.com>
2024-02-26 20:51:19 -0300

    Python

--- ex007.py
+++ ex007.py
@@ -1,14 +1,13 @@
 nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
-nota3 = float(input('Digite a terceira nota: '))
-nota4 = float(input('Digite a quarta nota: '))
 
-media = (nota1 + nota2 + nota3 + nota4) / 4
+media = (nota1 + nota2) / 2
 
-print('A media do aluno e {:.2}'.format(media))
+print('A media do aluno e {:.1f}'.format(media))
 
 if media < 5.0:
     print('REPROVADO')
 if media >= 5.00:
     print('APROVADO')
-    
+
+# {:.1f} Arredonda para cima de acordo com as regras algébricas
