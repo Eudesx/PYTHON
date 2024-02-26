f01d49604ef15665e81dba92c89d5bf4d6312b16
Eudesx <eudesvicente@rocketmail.com>
2024-02-26 20:06:21 -0300

    Python

--- ex005.py
+++ ex005.py
@@ -1,7 +1,3 @@
 n = int(input('Digite um valor: '))
 
-a = n - 1
-s = n + 1
-
-print('O valor digitado foi {} seu sucessor e {} e seu antecessor e {}'.format(n, s, a))
-
+print('O valor digitado foi {} seu sucessor e {} e seu antecessor e {}'.format(n, (n+1), (n-1)))
