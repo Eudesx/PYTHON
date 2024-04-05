2096af87c459047cd39c3da248f63e0c04f07152
Eudesx <eudesvicente@rocketmail.com>
2024-04-05 10:26:49 -0300

    Python

--- ex021.py
+++ ex021.py
@@ -1,5 +1,17 @@
 import pygame
+import sys
+
 pygame.init()
 pygame.mixer.music.load('ex021.mp3')
 pygame.mixer.music.play()
-pygame.event.wait()
+
+# Loop principal
+while pygame.mixer.music.get_busy():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.mixer.music.stop()
+            pygame.quit()
+            sys.exit()
+
+    pygame.time.Clock().tick(10)  # Limita a taxa de atualização do loop
+
