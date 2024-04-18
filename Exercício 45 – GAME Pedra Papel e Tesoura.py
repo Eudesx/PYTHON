cf68b612546a3037b998f43663f014adc459c341
Eudesx <eudesvicente@rocketmail.com>
2024-04-18 20:41:53 -0300

    Python

---
+++ Exercício 45 – GAME Pedra Papel e Tesoura.py
@@ -0,0 +1,47 @@
+from random import randint
+from time import sleep
+
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('''Suas opções:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] Tesoura''')
+jogador = int(input('Qual é sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('POO!!')
+print('-=' * 11)
+print('Computador jogou {}'.format(itens[computador]))
+print('Jogador jogou {}'.format(itens[jogador]))
+print('-=' * 11)
+
+if computador == 0: # Computador jogou pedra
+    if jogador == 0:
+        print('EMPATE')
+    elif jogador == 1:
+        print('JOGADOR VENCE')
+    elif jogador == 2:
+        print('COMPUTADOR VENCE')
+    else:
+        print('Jogada invalida')
+elif computador == 1: # computador jogou papel
+    if jogador == 0:
+        print('COMPUTADOR VENCE')
+    elif jogador == 1:
+        print('EMPATE')
+    elif jogador == 2:
+        print('JOGADOR VENCE')
+    else:
+        print('Jogada invalida')
+elif computador == 2: # computador jogou tesoura
+    if jogador == 0:
+        print('JOGADOR VENCE')
+    elif jogador == 1:
+        print('COMPUTADOR VENCE')
+    elif jogador == 2:
+        print('EMPATE')
+    else:
+        print('Jogada invalida')
