660b5a23e41ed47b44e1fcdd754ec85b7c4f4b63
Eudesx <eudesvicente@rocketmail.com>
2024-06-14 19:35:06 -0300

    Python

---
+++ Exercício 73 – Tuplas com Times de Futebol.py
@@ -0,0 +1,17 @@
+times = ('Corinthians', 'Palmeiras', 'Santos', 'Grêmio',
+         'Cruzeiro', 'Flamengo', 'Vasco', 'Chapecoense',
+         'Atlético', 'Botafogo', 'Atlético-PR', 'Bahia',
+         'São Paulo', 'Fluminense', 'Sport Recife',
+         'EC Vitória', 'Coritiba', 'Avaí', 'Ponte Preta',
+         'Atlético-GO')
+print('-=' * 15)
+print(f'Lista de times do Brasileirão {times}')
+print('-=' * 15)
+print(f'Os 5 primeiros são {times[0:5]}')
+print('-=' * 15)
+print(f'Os 4 últimos são {times[-4:]}')
+print('-=' * 15)
+print(f'Times em ordem alfabética: {sorted(times)}')
+print('-=' * 15)
+print(f' O chapecoense está na {times.index('Chapecoense')+1}ª posição')
+
