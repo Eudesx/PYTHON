aeef2edcf3e8b562f349777da3cf2a2958953373
Eudesx <eudesvicente@rocketmail.com>
2024-07-06 19:39:34 -0300

    Python

---
+++ Exercício 83 – Validando expressões matemáticas.py
@@ -0,0 +1,15 @@
+expr = str(input('Digite a expresão: '))
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expresão está valida!')
+else:
+    print('Sua expresão está errada!')
