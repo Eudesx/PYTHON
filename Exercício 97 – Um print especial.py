4b7efe2ea449734bddd46cfe0d95a6f48463b6e3
CYBER SECURITY <107135580+Eudesx@users.noreply.github.com>
2024-08-14 21:23:49 -0300

    Python

---
+++ Exercício 97 – Um print especial.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+# Programa principal
+escreva('Eudes  ')
+escreva('Curso de Python no Youtube')
+escreva('CeV')
