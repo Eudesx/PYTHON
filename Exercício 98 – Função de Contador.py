d1ecddc7d275a3794c55b89654a072966d1129a2
CYBER SECURITY <107135580+Eudesx@users.noreply.github.com>
2024-08-17 19:50:41 -0300

    Python

---
+++ Exercício 98 – Função de Contador.py
@@ -0,0 +1,37 @@
+from time import sleep
+
+
+def contador(i, f, p):
+    if p < 0:
+        p *= -1
+        if p == 0:
+            p = 1
+    print('-=' * 20)
+    print(f'Contagem de {i} até {f} de {p} em {p}')
+    sleep(2.5)
+
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.2)
+            cont += p
+        print('FIM')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ', end='', flush=True)
+            sleep(0.2)
+            cont -= p
+        print('FIM!')
+
+
+# Programa principal
+contador(1, 10, 1)
+contador(10, 0, 2)
+print('-=' * 20)
+print('Agora a sua vez de personalizar a contagem!')
+ini = int(input('Início: '))
+fim = int(input('FIM:    '))
+pas = int(input('Passo:  '))
+contador(ini, fim, pas)
