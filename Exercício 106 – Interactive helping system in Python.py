3e72380a04691628f747aa0ac776b976de735898
CYBER SECURITY <107135580+Eudesx@users.noreply.github.com>
2024-09-12 21:07:50 -0300

    Python

---
+++ Exercício 106 – Interactive helping system in Python.py
@@ -0,0 +1,35 @@
+from time import sleep
+
+c = ('\033[n'  # 0 - sem cores
+     '\033[0;30;41m'  # 1 - vermelho
+     )
+
+
+def ajuda(com):
+    titulo(f'Acessando o manual do comando \'{com}\'', 4)
+    print(c[6], end='')
+    help(com)
+    print(c[0], end='')
+    sleep(2)
+
+
+def titulo(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+    print(c[0], end='')
+    sleep(1)
+
+
+# Programa principal
+comando = ''
+while True:
+    titulo('SISTEMA DE AJUDA PYHELP', 1)
+    comando = str(input("Função ou Biblioteca > "))
+    if comando.upper() == 'FIM':
+        break
+    else:
+        ajuda(comando)
+titulo('ATÉ LOGO', 1)
